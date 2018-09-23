24139f1b5b384ae972fe352cd8e9eb794c2ba982
demelja <deemmel@gmail.com>
2018-09-23 08:20:58 +0200

    Initial commit

---
+++ app/src/main/java/module/mda/rhythmview/MainActivity.java
@@ -0,0 +1,14 @@
+package module.mda.rhythmview;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
